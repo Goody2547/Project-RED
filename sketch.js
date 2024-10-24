b144db36df6a850888eba7b7d61445fda0a84edc
Goody2547 <goodapirach@gmail.com>
2024-10-24 22:23:08 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,80 @@
+let x = 50;
+let y = 100;
+let z = 200;
+let a = 300;
+let b = 0;
+let c = 350;
+let goLeft = false;
+
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(220); 
+  
+  background("#b80000");  // Set the background color
+
+  // Make the color bars with rectangle shapes
+  fill("#620000"); noStroke(); rect(0, 0, 100, height);
+  fill("#840101"); noStroke(); rect(100, 0, 100, height);
+  fill("#9f0202"); noStroke(); rect(200, 0, 100, height);
+  fill("#c40101"); noStroke(); rect(300, 0, 100, height);
+  fill("#df0000"); noStroke(); rect(400, 0, 100, height);
+  fill("#ff0000"); noStroke(); rect(500, 0, 100, height);
+  
+  // Fill colors for "R" based on position X
+  if (x > 50 && x <= 300) {
+    fill("#28b800");  // Green for "R"
+  } else if (x > 300 && x <= 350) {
+    fill("#1b7f00");  // Dark green for "R"
+  } else if(x > 350 && x <= 400) {
+    fill("#135800");  // Darkest green for "R"
+  } else {
+    fill("#6eff45"); // Light green for "R"
+  }
+  textSize(300);
+  textFont("IMPACT");
+  textAlign(CENTER);
+  text("R", x, y, z);  // Make letter "R" with position X
+  
+  // Fill colors for "E" based on position A
+  if (a > 300 && a <= 350) {
+    fill("#1b7f00");  // Dark green for "E"
+  } else if (a > 350 && a <= 400) {
+    fill("#135800");  // Darkest green for "E"
+  } else {
+    fill("#28b800");  // Green for "E"
+  }
+  text("E", a, y, b);  // Make letter "E" with position A
+  
+  // Fill color for "D" based on position C
+  if (c > 350 && c <= 400) {
+    fill("#135800");  // Darkest green for "D"
+  } else if (c > 400 && c <= 500) {
+    fill("#28b800");  // Green for "D"
+  } else {
+    fill("#1b7f00");  // Dark green for "D"
+  }
+  text("D", c, y, z);  // Make letter "D" with position C
+  
+  // Move the letters from left to right
+  if (goLeft === false) {
+    x = x + 20;
+    a = a + 20;
+    c = c + 20;
+  } else {
+    x = x - 20;
+    a = a - 20;
+    c = c - 20;
+  }
+  
+  // Bounce when the letters hit the screen edges
+  if (x > 600 || a > 600 || c > 600) {
+    goLeft = true;
+  }
+  
+  if (x < 0 || a < 0 || c < 0) {
+    goLeft = false;
+  }
+}
